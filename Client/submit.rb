ffa81ff8914aee1d57eff5a96a5b556ee17dc346
seshin <sam@baremetalnetworks.com>
2015-10-22 20:41:48 -0700

    dfgdg

--- Client/submit.rb
+++ Client/submit.rb
@@ -1,3 +1,66 @@
+
+options = Hash.new
+options[:host] = 'clusterforge.us'
+options[:port] = '7000'
+options[:user] = 'foo'
+options[:pass] = 'bar'
+options[:version] = 'submit-harness-pre-0.2.0'
+options[:cid] = 'cid400f0sfs053353325235325235'
+
+
+class Core
+	attr_writer :elogger, :logger
+
+	def initialize
+		@elogger = Logger.new($OPT[:error_log], 'weekly')
+		@elogger.level = Logger::WARN
+
+		@logger = Logger.new($OPT[:log_file], 'weekly')
+		@logger.level = Logger::WARN
+	end
+end
+
+class Instance < Core
+	attr_accessor :log_location, :pcap_location, :strace_location,
+	              :instance_type, :status, :gucid, :ban_duration,
+	              :hostname, :pcap_port, :pcap_filter, :pcap_interface,
+	              :pcap_thread_flag, :message, :stats_log, :stats_pcap,
+	              :monitor_log, :monitor_pcap, :sniffer_thread
+
+	def initialize(ins_conf)
+		@gucid = ins_conf[:gucid]
+		@pcap_thread_flag = 0
+#		@hostname = Socket.gethostname
+		@message = ""
+	end
+	class Core
+		attr_writer :elogger, :logger
+
+		def initialize
+			@elogger = Logger.new($OPT[:error_log], 'weekly')
+			@elogger.level = Logger::WARN
+
+			@logger = Logger.new($OPT[:log_file], 'weekly')
+			@logger.level = Logger::WARN
+		end
+	end
+
+	class Instance < Core
+		attr_accessor :log_location, :pcap_location, :strace_location,
+		              :instance_type, :status, :gucid, :ban_duration,
+		              :hostname, :pcap_port, :pcap_filter, :pcap_interface,
+		              :pcap_thread_flag, :message, :stats_log, :stats_pcap,
+		              :monitor_log, :monitor_pcap, :sniffer_thread
+
+		def initialize(ins_conf)
+			@gucid = ins_conf[:gucid]
+			@pcap_thread_flag = 0
+#		@hostname = Socket.gethostname
+			@message = ""
+		end
+
+def submit_to_switchyard(logfile, pcapfile, options)
+
 begin
 	uri = URI.parse('https://' + options[:host] + ':' +
 			                options[:port] + '/submit')
